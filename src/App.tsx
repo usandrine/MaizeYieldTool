1ee179d02958d85389d7f298847c415b88b43a43
Umugwaneza sandrine <umusandrine001@gmail.com>
2025-04-19 12:04:04 +0200

    fisrt commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from "./pages/LandingPage"
+
+function App() {
+  return (
+    <div className="bg-blue-500">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
